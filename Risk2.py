94385b4277a59dd6e26be3a7028f11339b9f29fe
ethaneig <122510172+ethaneig@users.noreply.github.com>
2024-03-23 12:04:45 -0500

    Create Game

---
+++ Risk2.py
@@ -0,0 +1,21 @@
+
+import random
+import numpy as np
+
+
+
+class Board:
+    
+
+    __init__(self, size):
+        self.size = size
+        self.countries = []
+
+
+class Country:
+
+
+    __init__(self, name, player, connections):
+        self.name = name
+        self.player = player
+        self.connections = {}
