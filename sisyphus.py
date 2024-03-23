9f64b3487d99eea8b11ab279e56b98eb17ccafb1
Ron Kiehn <ronthekiehn@gmail.com>
2024-03-23 15:05:43 -0500

    boulder :(

---
+++ sisyphus.py
@@ -0,0 +1,167 @@
+import os
+import time
+import random
+
+def main(): 
+    rand_list=[]
+    n=5
+    for i in range(n):
+        rand_list.append(random.randint(1,9))
+
+    checker = rand_list[:]
+    checker.sort()
+    while (checker != rand_list):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+        timer = 0.1
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ###")
+        print("        ###-|#########          ####")
+        print("        ##-/##########      ###")
+        print("        #    ########   ####")
+        print("       # #    ##### ###")
+        print("     #   #  ###")
+        print("   #    ###      ")
+        print("   ###")
+        print("###")
+        time.sleep(timer)
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ####")
+        print("        ###-|#########         ####")
+        print("        ##-/##########      ###")
+        print("        #    ########   ####")
+        print("       # #    #####  ###")
+        print("     #  #   ####")
+        print("        ####      ")
+        print("   #####")
+        print("###")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ###")
+        print("        ###-|#########          ####")
+        print("        ##-/##########      ###")
+        print("        #    ########   ###")
+        print("       ##     ##### ####")
+        print("      # #   #####")
+        print("      # ####       ")
+        print("   ####")
+        print("###")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ####")
+        print("        ###-|#########          ###")
+        print("        ##-/##########      ####")
+        print("        #    ########   ###")
+        print("       ##     ##### ####")
+        print("       ##   ####")
+        print("       #####      ")
+        print("   ####")
+        print("###")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ###")
+        print("        ###-|#########          #####")
+        print("        ##-/##########      ####")
+        print("        #    ########   ####")
+        print("        #     ##### ###")
+        print("        ##   #####")
+        print("       ####       ")
+        print("   ###")
+        print("###")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+        
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ###")
+        print("        ###-|#########          ####")
+        print("        ##-/##########      ###")
+        print("        #    ########   ###")
+        print("       ##     ##### ####")
+        print("      # #   #####")
+        print("      # ####       ")
+        print("   ####")
+        print("#####")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ####")
+        print("        ###-|#########         ######")
+        print("        ##-/##########      ###")
+        print("        #    ########   ####")
+        print("       # #    #####  ###")
+        print("     #   #  ####  ###")
+        print("    #    ####      ")
+        print("   #####")
+        print("###")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lister(rand_list)
+
+        print("                ##")
+        print("              ######")
+        print("         #   ########                ######")
+        print("        ###-|#########          ####")
+        print("        ##-/##########      ###")
+        print("        #    ########   ####")
+        print("       # #    ##### ###")
+        print("     #    # ###")
+        print("   #    ####     ")
+        print("   ###")
+        print("###")
+
+        time.sleep(timer)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    for num in rand_list:
+        for hash in range(num):
+            print("#",end="")
+        print()
+
+    print("                ##")
+    print("              ######")
+    print("         #   ########                ######")
+    print("        ###-|#########          ####")
+    print("        ##-/##########      ###")
+    print("        #    ########   ####")
+    print("       # #    ##### ###")
+    print("     #    # ###")
+    print("   #    ####     ")
+    print("   ###")
+    print("###")
+
+        
+
+
+def lister(rand_list):
+    for num in rand_list:
+        for hash in range(num):
+            print("#",end="")
+        print()
+    random.shuffle(rand_list)
+
+main()
