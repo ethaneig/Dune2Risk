97fda3ea116904553137daf9b3cae80d7390a296
mibr4601 <mtbrown@uchicago.edu>
2024-03-24 02:35:21 -0500

    did colors

--- hud.py
+++ hud.py
@@ -7,6 +7,8 @@ import copy
 HUD_WIDTH = 250
 HUD_HEIGHT = 600
 HUD_BG_COLOR = (200, 200, 200)
+playercolors = [(255, 0, 0), (0,0,0), (255,105,180), (0, 0, 255)]
+
 
 class Sound:
   def __init__(self,path):
@@ -107,18 +109,19 @@ def draw_hud(screen, phase, player_turn):
     hud_surface.fill(HUD_BG_COLOR)
     # Add text to the HUD (for demonstration purposes)
     font = pygame.font.Font(None, 24)
-    text_surface = font.render(f"Player {player_turn + 1}'s turn", True, (0, 0, 0))
+    color = playercolors[player_turn]
+    text_surface = font.render(f"Player {player_turn + 1}'s turn", True, color)
     text_rect = text_surface.get_rect(center=(HUD_WIDTH // 2, 50))
     hud_surface.blit(text_surface, text_rect)
     if phase == 0:
-        text_surface = font.render("Place Troops", True, (0, 0, 0))
+        text_surface = font.render("Place Troops", True, color)
     else:
-       text_surface = font.render("Choose Territory to Attack", True, (0, 0, 0))
+       text_surface = font.render("Choose Territory to Attack", True, color)
     text_rect = text_surface.get_rect(center=(HUD_WIDTH // 2, 100))
     hud_surface.blit(text_surface, text_rect)
 
     pygame.draw.rect(hud_surface, (255, 0, 0), (50, 300, 150, 50))
-    text_surface = font.render("End Action", True, (0, 0, 0))
+    text_surface = font.render("End Action", True, (0,0,0))
     text_rect = text_surface.get_rect(center=(HUD_WIDTH // 2, 325))
     hud_surface.blit(text_surface, text_rect)
     # Blit the HUD onto the screen
